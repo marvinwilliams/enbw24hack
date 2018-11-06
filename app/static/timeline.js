75b33bb39ed79c6d8f6919ba4262dcbe225eee02
froleyks <nfroleyks@gmail.com>
2018-11-06 06:56:42 +0100

    blupFix and formatting

--- app/static/timeline.js
+++ app/static/timeline.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
         var text = "All ok";
         var color = "green";
         switch (status) {
-        case 1:
-            text = "Error";
-            color = "red";
-            break;
-        case 0:
-            text = "All ok!";
-            color = "green";
-            break;
+            case 1:
+                text = "Error";
+                color = "red";
+                break;
+            case 0:
+                text = "All ok!";
+                color = "green";
+                break;
         }
         $("#status").html($("<div>").append($("<h3>").text("Status: " + text)).css("color", color));
     };
@@ -42,12 +42,12 @@ $(document).ready(function() {
 
     // makes timeline clickable
     timeline.addEventListener("click", function(event) {
-	moveplayhead(event);
+        moveplayhead(event);
     }, false);
 
     // returns click as decimal (.77) of the total timelineWidth
     function clickPercent(event) {
-	return (event.clientX - getPosition(timeline)) / timelineWidth;
+        return (event.clientX - getPosition(timeline)) / timelineWidth;
     }
 
     // makes playhead draggable
@@ -59,37 +59,37 @@ $(document).ready(function() {
 
     // mouseDown EventListener
     function mouseDown() {
-	onplayhead = true;
-	window.addEventListener('mousemove', moveplayhead, true);
+        onplayhead = true;
+        window.addEventListener('mousemove', moveplayhead, true);
     }
 
     // mouseUp EventListener
     // getting input from all mouse clicks
     function mouseUp(event) {
-	if (onplayhead == true) {
-	    moveplayhead(event);
-	    window.removeEventListener('mousemove', moveplayhead, true);
-	    // change current time
-	}
-	onplayhead = false;
+        if (onplayhead == true) {
+            moveplayhead(event);
+            window.removeEventListener('mousemove', moveplayhead, true);
+            // change current time
+        }
+        onplayhead = false;
     }
     // mousemove EventListener
     // Moves playhead as user drags
     function moveplayhead(event) {
-	var newMargLeft = event.clientX - getPosition(timeline);
+        var newMargLeft = event.clientX - getPosition(timeline);
 
-	if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-	    playhead.style.marginLeft = newMargLeft + "px";
-	    currentTime = (timelineWidth * (newMargLeft / duration));
-	}
-	if (newMargLeft < 0) {
-	    playhead.style.marginLeft = "0px";
-	    currentTime = 0;
-	}
-	if (newMargLeft > timelineWidth) {
-	    playhead.style.marginLeft = timelineWidth + "px";
-	    currentTime = duration;
-	}
+        if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+            playhead.style.marginLeft = newMargLeft + "px";
+            currentTime = (timelineWidth * (newMargLeft / duration));
+        }
+        if (newMargLeft < 0) {
+            playhead.style.marginLeft = "0px";
+            currentTime = 0;
+        }
+        if (newMargLeft > timelineWidth) {
+            playhead.style.marginLeft = timelineWidth + "px";
+            currentTime = duration;
+        }
     }
 
     // timeUpdate
@@ -115,34 +115,30 @@ $(document).ready(function() {
         }
 
         var time = $("<div>").append($("<h3>").text(timeString));
-	      var playPercent = timelineWidth * (currentTime / duration);
-	      playhead.style.marginLeft = playPercent + "px";
-	      $.post( "/ticktock", {
-	          javascript_data: blub 
-	      });
-
+        var playPercent = timelineWidth * (currentTime / duration);
+        playhead.style.marginLeft = playPercent + "px";
         $("#clock").html(time);
-	playhead.style.marginLeft = playPercent + "px";
-	$.post( "/ticktock", {
-	    javascript_data: currentTime,
-	    success: function() {
-		$.ajax({
-		    url: "/eval_network",
-		    dataType: 'json',
-		    contentType: 'application/json',
-		    mimeType: 'application/json',
-		    success: function(data) {
-			(source.getFeatures()).forEach(function(f) {
-			    for (var i = 0; i < data[1].length; i++) {
-				if (data[1][i].id == f.O.id) {
-				    f.flow = data[1][i].flow;
-				}
-			    }
-			});
-		    }
-		});
-	    }
-	});
+        playhead.style.marginLeft = playPercent + "px";
+        $.post("/ticktock", {
+            javascript_data: currentTime,
+            success: function() {
+                $.ajax({
+                    url: "/eval_network",
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    mimeType: 'application/json',
+                    success: function(data) {
+                        (source.getFeatures()).forEach(function(f) {
+                            for (var i = 0; i < data[1].length; i++) {
+                                if (data[1][i].id == f.O.id) {
+                                    f.flow = data[1][i].flow;
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
     //Play and Pause
@@ -170,18 +166,18 @@ $(document).ready(function() {
                 timeUpdate();
             }, 2000);
 
-	} else { // pause music
-	    // remove pause, add play
-	    paused = true;
-	    pButton.className = "";
-	    pButton.className = "play";
-	    clearInterval(interval);
-	    interval = 0;
-	}
+        } else { // pause music
+            // remove pause, add play
+            paused = true;
+            pButton.className = "";
+            pButton.className = "play";
+            clearInterval(interval);
+            interval = 0;
+        }
     }
 
     // Returns elements left position relative to top-left of viewport
     function getPosition(el) {
-	return el.getBoundingClientRect().left;
+        return el.getBoundingClientRect().left;
     }
 });
